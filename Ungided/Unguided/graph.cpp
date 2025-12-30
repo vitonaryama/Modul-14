aa445ae5d1131c2b3200bf0ceba14d67512308fa
vitonaryama <101534475+vitonaryama@users.noreply.github.com>
2025-12-30 23:00:25 +0700

    Add files via upload

---
+++ Ungided/Unguided/graph.cpp
@@ -0,0 +1,202 @@
+#include "graph.h"
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+void CreateGraph(Graph &G) {
+    G.First = NULL;
+}
+
+adrNode AlokasiNode(infoGraph data) {
+    adrNode nodeBaru = new ElmNode;
+    nodeBaru->info = data;
+    nodeBaru->visited = 0;
+    nodeBaru->firstEdge = NULL;
+    nodeBaru->Next = NULL;
+    return nodeBaru;
+}
+
+adrEdge AlokasiEdge(adrNode nodeTujuan) {
+    adrEdge edgeBaru = new ElmEdge;
+    edgeBaru->Node = nodeTujuan;
+    edgeBaru->Next = NULL;
+    return edgeBaru;
+}
+
+void InsertNode(Graph &G, infoGraph data) {
+    adrNode nodeBaru = AlokasiNode(data);
+    if (G.First == NULL) {
+        G.First = nodeBaru;
+    } else {
+        adrNode nodeBantu = G.First;
+        while (nodeBantu->Next != NULL) {
+            nodeBantu = nodeBantu->Next;
+        }
+        nodeBantu->Next = nodeBaru;
+    }
+}
+
+adrNode FindNode(Graph G, infoGraph data) {
+    adrNode nodeBantu = G.First;
+    while (nodeBantu != NULL) {
+        if (nodeBantu->info == data) {
+            return nodeBantu;
+        }
+        nodeBantu = nodeBantu->Next;
+    }
+    return NULL;
+}
+
+void ConnectNode(Graph &G, infoGraph info1, infoGraph info2) {
+    adrNode node1 = FindNode(G, info1);
+    adrNode node2 = FindNode(G, info2);
+
+    if (node1 != NULL && node2 != NULL) {
+        adrEdge Edge1 = AlokasiEdge(node2);
+        Edge1->Next = node1->firstEdge;
+        node1->firstEdge = Edge1;
+
+        adrEdge Edge2 = AlokasiEdge(node1);
+        Edge2->Next = node2->firstEdge;
+        node2->firstEdge = Edge2;
+    } else {
+        cout << "Node tidak ditemukan!" << endl;
+    }
+}
+
+void DisconnectNode(adrNode node1, adrNode node2) {
+    if (node1 != NULL && node2 != NULL) {
+        adrEdge edgeBantu = node1->firstEdge;
+        adrEdge PrevE = NULL;
+
+        while (edgeBantu != NULL && edgeBantu->Node != node2) {
+            PrevE = edgeBantu;
+            edgeBantu = edgeBantu->Next;
+        }
+
+        if (edgeBantu != NULL) {
+            if (PrevE == NULL) {
+                node1->firstEdge = edgeBantu->Next;
+            } else {
+                PrevE->Next = edgeBantu->Next;
+            }
+            delete edgeBantu;
+        }
+    }
+}
+
+void DeleteNode(Graph &G, infoGraph X) {
+    adrNode nodeHapus = FindNode(G, X);
+    if (nodeHapus == NULL) {
+        cout << "Node tidak ditemukan." << endl;
+        return;
+    }
+
+    adrNode nodeLainnya = G.First;
+    while (nodeLainnya != NULL) {
+        DisconnectNode(nodeLainnya, nodeHapus);
+        nodeLainnya = nodeLainnya->Next;
+    }
+
+    adrEdge edgeBantu = nodeHapus->firstEdge;
+    while (edgeBantu != NULL) {
+        adrEdge tempE = edgeBantu;
+        edgeBantu = edgeBantu->Next;
+        delete tempE;
+    }
+    nodeHapus->firstEdge = NULL;
+
+    if (G.First == nodeHapus) {
+        G.First = nodeHapus->Next;
+    } else {
+        adrNode nodeBantu = G.First;
+        while (nodeBantu->Next != nodeHapus) {
+            nodeBantu = nodeBantu->Next;
+        }
+        nodeBantu->Next = nodeHapus->Next;
+    }
+
+    delete nodeHapus;
+}
+
+void PrintInfoGraph(Graph G) {
+    adrNode nodeBantu = G.First;
+    while (nodeBantu != NULL) {
+        cout << "Node " << nodeBantu->info << " terhubung ke: ";
+        adrEdge edgeBantu = nodeBantu->firstEdge;
+        while (edgeBantu != NULL) {
+            cout << edgeBantu->Node->info << " ";
+            edgeBantu = edgeBantu->Next;
+        }
+        cout << endl;
+        nodeBantu = nodeBantu->Next;
+    }
+}
+
+void ResetVisited(Graph &G) {
+    adrNode nodeReset = G.First;
+    while (nodeReset != NULL) {
+        nodeReset->visited = 0;
+        nodeReset = nodeReset->Next;
+    }
+}
+
+void PrintBFS(Graph G, infoGraph StartInfo) {
+    ResetVisited(G);
+    adrNode StartNode = FindNode(G, StartInfo);
+    
+    if (StartNode == NULL) return;
+
+    queue<adrNode> Qyu;
+    Qyu.push(StartNode);
+    StartNode->visited = 1;
+
+    cout << "BFS Traversal: ";
+    while (!Qyu.empty()) {
+        adrNode nodeBantu = Qyu.front();
+        Qyu.pop();
+        cout << nodeBantu->info << " - ";
+
+        adrEdge edgeBantu = nodeBantu->firstEdge;
+        while (edgeBantu != NULL) {
+            if (edgeBantu->Node->visited == 0) {
+                edgeBantu->Node->visited = 1;
+                Qyu.push(edgeBantu->Node);
+            }
+            edgeBantu = edgeBantu->Next;
+        }
+    }
+    cout << endl;
+}
+
+void PrintDFS(Graph G, infoGraph StartInfo) {
+    ResetVisited(G);
+    adrNode StartNode = FindNode(G, StartInfo);
+    
+    if (StartNode == NULL) return;
+
+    stack<adrNode> Stak;
+    Stak.push(StartNode);
+
+    cout << "DFS Traversal: ";
+    while (!Stak.empty()) {
+        adrNode nodeBantu = Stak.top();
+        Stak.pop();
+
+        if (nodeBantu->visited == 0) {
+            nodeBantu->visited = 1;
+            cout << nodeBantu->info << " - ";
+
+            adrEdge edgeBantu = nodeBantu->firstEdge;
+            while (edgeBantu != NULL) {
+                if (edgeBantu->Node->visited == 0) {
+                    Stak.push(edgeBantu->Node);
+                }
+                edgeBantu = edgeBantu->Next;
+            }
+        }
+    }
+    cout << endl;
+}
---
+++ Ungided/Unguided/main.cpp
@@ -0,0 +1,56 @@
+#include "graph.h"
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    Graph G;
+    CreateGraph(G);
+
+    InsertNode(G, 'A');
+    InsertNode(G, 'B');
+    InsertNode(G, 'C');
+    InsertNode(G, 'D');
+    InsertNode(G, 'E');
+    InsertNode(G, 'F');
+
+    ConnectNode(G, 'A', 'B');
+    ConnectNode(G, 'A', 'D');
+    ConnectNode(G, 'B', 'C');
+    ConnectNode(G, 'D', 'C');
+    ConnectNode(G, 'B', 'E');
+    ConnectNode(G, 'C', 'E');
+    ConnectNode(G, 'C', 'F');
+    ConnectNode(G, 'E', 'F');
+
+    cout << "=== REPRESENTASI ADJACENCY LIST ===" << endl;
+    PrintInfoGraph(G);
+    cout << endl;
+
+    cout << "=== HASIL TRAVERSAL ===" << endl;
+    PrintBFS(G, 'A');
+    PrintDFS(G, 'A');
+    cout << endl;
+
+    cout << "=== HAPUS NODE E ===" << endl;
+    DeleteNode(G, 'E');
+
+    if (FindNode(G, 'E') == NULL) {
+        cout << "Node E berhasil terhapus" << endl;
+    } else {
+        cout << "Node E tidak berhasil terhapus" << endl;
+    }
+
+    cout << endl;
+    cout << "=== REPRESENTASI ADJACENCY LIST ===" << endl;
+    PrintInfoGraph(G);
+    cout << endl;
+
+    cout << "=== HASIL TRAVERSAL ===" << endl;
+    PrintBFS(G, 'A');
+    PrintDFS(G, 'A');
+
+    return 0;
+}
